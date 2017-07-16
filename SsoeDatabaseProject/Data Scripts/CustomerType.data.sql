f440b8391619c2dcc250510954c211e3ee5c1d11
DarthVader\proffy <proffy@comcast.net>
2017-07-16 06:14:07 -0700

    Adding Post deployment Script and data script

---
+++ SsoeDatabaseProject/Data Scripts/CustomerType.data.sql
@@ -0,0 +1,5 @@
+﻿delete from [dbo].[CustomerType]
+
+INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [Type]) VALUES (1, N'Corporation')
+INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [Type]) VALUES (2, N'Individual')
+INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [Type]) VALUES (3, N'Educator')
